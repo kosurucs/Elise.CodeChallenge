fccfdaf8be364f23612ceaa451ca5f1660a47f60
chandrasekhar <k.chandrasekhargowda@hotmail.com>
2020-06-26 21:48:19 +0530

    ChangeSet-26June2020

--- Elise.CodeChallenge.PartTwo.Server/Interfaces/IModelValidationService.cs
+++ Elise.CodeChallenge.PartTwo.Server/Interfaces/IModelValidationService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Elise.CodeChallenge.PartTwo.Server
+namespace Elise.CodeChallenge.PartTwo.Server.Interfaces
 {
-    public class Class1
+    public interface IModelValidationService
     {
     }
 }
---
+++ Elise.CodeChallenge.PartTwo.Server/Services/ModelValidationService.cs
@@ -0,0 +1,14 @@
+﻿using Elise.CodeChallenge.PartTwo.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elise.CodeChallenge.PartTwo.Server.Services
+{
+    public class ModelValidationService : IModelValidationService
+    {
+    }
+}
+
